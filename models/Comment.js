76d819cec163f18efcb5183a07b9aae087c1025e
Marrione Nguyen <mtiamsic@gmail.com>
2020-10-16 16:46:57 -0700

    completed

--- models/Comment.js
+++ models/Comment.js
@@ -6,6 +6,32 @@ class Comment extends Model {}
 Comment.init(
     {
         // columns will go here
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            len: [1]
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
     },
     {
         sequelize,
